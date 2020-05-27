9a8308a8fe5a10f165528a221e1c4718f8f5f48e
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-05-28 01:59:37 +0530

    Implement BFS

--- sitemapbuilder/main.go
+++ sitemapbuilder/main.go
@@ -11,19 +11,35 @@ import (
 	"github.com/pratikms/gophercises/linkparser"
 )
 
+func withPrefix(pfx string) func(string) bool {
+	return func(link string) bool {
+		return strings.HasPrefix(link, pfx)
+	}
+}
+
+func filter(links []string, keepFn func(string) bool) []string {
+	var ret []string
+	for _, link := range links {
+		if keepFn(link) {
+			ret = append(ret, link)
+		}
+	}
+	return ret
+}
+
 func hrefs(body io.Reader, base string) []string {
-	var hrefs []string
+	var ret []string
 	links, _ := linkparser.Parse(body)
 	for _, l := range links {
 		switch {
 		case strings.HasPrefix(l.Href, "/"):
-			hrefs = append(hrefs, base+l.Href)
+			ret = append(ret, base+l.Href)
 		case strings.HasPrefix(l.Href, "http"):
-			hrefs = append(hrefs, l.Href)
+			ret = append(ret, l.Href)
 		}
 	}
 
-	return hrefs
+	return ret
 }
 
 func get(urlStr string) []string {
@@ -40,17 +56,45 @@ func get(urlStr string) []string {
 		Host:   reqURL.Host,
 	}
 	base := baseURL.String()
-	fmt.Println("Request URL: ", reqURL)
-	fmt.Println("Base URL: ", base)
+	// fmt.Println("Request URL: ", reqURL)
+	// fmt.Println("Base URL: ", base)
 
-	return hrefs(resp.Body, base)
+	return filter(hrefs(resp.Body, base), withPrefix(base))
+}
+
+func bfs(urlStr string, maxDepth int) []string {
+	seen := make(map[string]struct{})
+	var q map[string]struct{}
+	nq := map[string]struct{}{
+		urlStr: struct{}{},
+	}
+	for i := 0; i <= maxDepth; i++ {
+		q, nq = nq, make(map[string]struct{})
+		for url, _ := range q {
+			if _, ok := seen[url]; ok {
+				continue
+			}
+			seen[url] = struct{}{}
+			for _, link := range get(url) {
+				nq[link] = struct{}{}
+			}
+		}
+	}
+
+	ret := make([]string, 0, len(seen))
+	for url, _ := range seen {
+		ret = append(ret, url)
+	}
+
+	return ret
 }
 
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "the URL that you want to build a sitemap for")
+	maxDepth := flag.Int("depth", 3, "the maximum depth to traverse")
 	flag.Parse()
 
-	pages := get(*urlFlag)
+	pages := bfs(*urlFlag, *maxDepth)
 	for _, href := range pages {
 		fmt.Println(href)
 	}
