f8251772492e2154d90c01d9fcdf75fec54db493
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-05-28 00:35:24 +0530

    Parsing and building links

--- sitemapbuilder/main.go
+++ sitemapbuilder/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
+
+	"github.com/pratikms/gophercises/linkparser"
 )
 
 func main() {
@@ -18,5 +20,28 @@ func main() {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+
+	reqURL := resp.Request.URL
+	baseURL := &url.URL{
+		Scheme: reqURL.Scheme,
+		Host:   reqURL.Host,
+	}
+	base := baseURL.String()
+	fmt.Println("Request URL: ", reqURL)
+	fmt.Println("Base URL: ", base)
+
+	var hrefs []string
+	links, _ := linkparser.Parse(resp.Body)
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
 }
