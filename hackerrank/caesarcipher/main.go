fe0837dffbaf3d3e5a00526cd8fe3d97b72ac643
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-05-31 00:54:56 +0530

    Add rune based solution

--- hackerrank/caesarcipher/main.go
+++ hackerrank/caesarcipher/main.go
@@ -2,20 +2,35 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func rotate(s rune, delta int, key []rune) rune {
-	idx := strings.IndexRune(string(key), s)
-	if idx < 0 {
-		panic("Rune not found in key")
+func rotate(r rune, base, delta int) rune {
+	tmp := int(r) - base
+	tmp = (tmp + delta) % 26
+	return rune(tmp + base)
+}
+
+func cipher(r rune, delta int) rune {
+	if r >= 'A' && r <= 'Z' {
+		return rotate(r, 'A', delta)
+	} else if r >= 'a' && r <= 'z' {
+		return rotate(r, 'a', delta)
+	} else {
+		return r
 	}
-	idx = (idx + delta) % len(key)
-	return key[idx]
 }
 
 func main() {
-	alphabet := []rune("abcdefghijklmnopqrstuvwxyz")
-	newRune := rotate('z', 2, alphabet)
-	fmt.Println(string(newRune))
+	var length, delta int
+	var input string
+	fmt.Scanf("%d\n", &length)
+	fmt.Scanf("%s\n", &input)
+	fmt.Scanf("%d\n", &delta)
+
+	var ret []rune
+	for _, ch := range input {
+		ret = append(ret, cipher(ch, delta))
+	}
+
+	fmt.Println(string(ret))
 }
