f73599e2821b6508d18f4361b9af49ec9eae94d3
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-07-30 01:22:57 +0530

    Refactor

--- phonenumbernormalizer/main.go
+++ phonenumbernormalizer/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"regexp"
 
-	_ "github.com/lib/pq"
+	"github.com/pratikms/gophercises/phonenumbernormalizer/db"
 )
-
-type Phone struct {
-	Id     int
-	Number string
-}
 
 const (
 	host     = "localhost"
@@ -43,149 +37,34 @@ func must(err error) {
 	}
 }
 
-func getPhone(db *sql.DB, id int) (string, error) {
-	var number string
-	row := db.QueryRow("SELECT * FROM phone_numbers WHERE id=$1", id)
-	err := row.Scan(&id, &number)
-	if err != nil {
-		return "", err
-	}
-	return number, nil
-}
-
-func findPhone(db *sql.DB, number string) (*Phone, error) {
-	var p Phone
-	row := db.QueryRow("SELECT * FROM phone_numbers WHERE value=$1", number)
-	err := row.Scan(&p.Id, &p.Number)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return nil, nil
-		}
-		return nil, err
-	}
-	return &p, nil
-}
-
-func updatePhone(db *sql.DB, p Phone) error {
-	statement := `UPDATE phone_numbers SET value=$2 where id=$1`
-	_, err := db.Exec(statement, p.Id, p.Number)
-	return err
-}
-
-func deletePhone(db *sql.DB, id int) error {
-	statement := `DELETE FROM phone_numbers WHERE id=$1`
-	_, err := db.Exec(statement, id)
-	return err
-}
-
-func allPhones(db *sql.DB) ([]Phone, error) {
-	rows, err := db.Query("SELECT id, value FROM phone_numbers")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var ret []Phone
-	for rows.Next() {
-		var p Phone
-		if err := rows.Scan(&p.Id, &p.Number); err != nil {
-			return nil, err
-		}
-		ret = append(ret, p)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-	return ret, nil
-}
-
-func insertPhone(db *sql.DB, phone string) (int, error) {
-	var id int
-	statement := `INSERT INTO phone_numbers(value) VALUES ($1) RETURNING id`
-	err := db.QueryRow(statement, phone).Scan(&id)
-	if err != nil {
-		return -1, err
-	}
-	return id, nil
-}
-
-func createPhoneNumbersTable(db *sql.DB) error {
-	statement := `
-	CREATE TABLE IF NOT EXISTS phone_numbers (
-		id SERIAL,
-		value VARCHAR(255)
-	)`
-	_, err := db.Exec(statement)
-	return err
-}
-
-func createDB(db *sql.DB, name string) error {
-	_, err := db.Exec("CREATE DATABASE " + name)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func resetDB(db *sql.DB, name string) error {
-	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
-	if err != nil {
-		return err
-	}
-	return createDB(db, name)
-}
-
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
-	db, err := sql.Open("postgres", psqlInfo)
-	must(err)
-	err = resetDB(db, dbname)
-	must(err)
-	db.Close()
+	must(db.Reset("postgres", psqlInfo, dbname))
 
 	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
-	db, err = sql.Open("postgres", psqlInfo)
-	must(err)
+	must(db.Migrate("postgres", psqlInfo))
 
+	db, err := db.Open("postgres", psqlInfo)
+	must(err)
 	defer db.Close()
 
-	must(createPhoneNumbersTable(db))
-
-	_, err = insertPhone(db, " 1234567890")
-	must(err)
-	_, err = insertPhone(db, "123 456 7891")
-	must(err)
-	_, err = insertPhone(db, "(123) 456 7892")
-	must(err)
-	_, err = insertPhone(db, "(123) 456-7893")
-	must(err)
-	_, err = insertPhone(db, "123-456-7894")
-	must(err)
-	_, err = insertPhone(db, "123-456-7890")
-	must(err)
-	_, err = insertPhone(db, "1234567892")
-	must(err)
-	id, err := insertPhone(db, "(123)456-7892")
+	err = db.Seed()
 	must(err)
 
-	number, err := getPhone(db, id)
-	must(err)
-	fmt.Println("Number is: ", number)
-
-	phones, err := allPhones(db)
+	phones, err := db.AllPhones()
 	must(err)
 	for _, p := range phones {
 		fmt.Printf("Processing %+v\n", p)
 		number := normalize(p.Number)
 		if number != p.Number {
 			fmt.Println("Updating or removing ", number)
-			existing, err := findPhone(db, number)
+			existing, err := db.FindPhone(number)
 			must(err)
 			if existing != nil {
-				must(deletePhone(db, p.Id))
+				must(db.DeletePhone(p.ID))
 			} else {
 				p.Number = number
-				must(updatePhone(db, p))
+				must(db.UpdatePhone(&p))
 			}
 		} else {
 			fmt.Println("No changes required")
