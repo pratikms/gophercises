3a31ab228757eec7c68fb7f83dda7c5b5979aaa7
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2021-07-12 23:12:35 +0530

    Add sort functionality

--- deckofcards/card_test.go
+++ deckofcards/card_test.go
@@ -26,3 +26,19 @@ func TestNew(t *testing.T) {
 		t.Error("Wrong number of cards in the new deck")
 	}
 }
+
+func TestDefaultSort(t *testing.T) {
+	cards := New(DefaultSort)
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("Expected: Ace of Spade. Received: ", cards[0])
+	}
+}
+
+func TestSort(t *testing.T) {
+	cards := New(Sort(Less))
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("Expected: Ace of Spade. Received: ", cards[0])
+	}
+}
