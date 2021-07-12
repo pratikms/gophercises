74c8a0180292a40e1948ee1f9da7b4f0283b76fc
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2021-07-12 22:21:20 +0530

    Add new deck of card feature

--- deckofcards/card.go
+++ deckofcards/card.go
@@ -38,9 +38,26 @@ type Card struct {
 	Rank
 }
 
+var suits = [...]Suit{Spade, Diamond, Club, Heart}
+
+const (
+	minRank = Ace
+	maxRank = King
+)
+
 func (c Card) String() string {
 	if c.Suit == Joker {
 		return c.Suit.String()
 	}
 	return fmt.Sprintf("%s of %ss", c.Rank.String(), c.Suit.String())
+}
+
+func New() []Card {
+	var cards []Card
+	for _, suit := range suits {
+		for rank := minRank; rank <= maxRank; rank++ {
+			cards = append(cards, Card{Suit: suit, Rank: rank})
+		}
+	}
+	return cards
 }
